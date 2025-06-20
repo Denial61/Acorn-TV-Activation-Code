d727707be0c5492741cb410648aba513c597e424
Denial61 <hs51525254@gmail.com>
2025-06-20 18:02:47 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,15 @@
- project = ''
-
+project = '-'
 author = 'Your Name'
-
 release = '1.0'
  
 extensions = []
-
 templates_path = ['_templates']
-
 exclude_patterns = []
  
 html_theme = 'alabaster'
-
-html_static_path = ['_static']
- 
+html_static_path = ['_static']   
  
 html_js_files = [
-
-    'chat.js',]
- 
- 
+    'chatbot.js',
+] 
 html_favicon = '_static/acorn-favicon.jpg'
